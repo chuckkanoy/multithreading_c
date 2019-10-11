f94a5cfa77f09f0e25cc964a8b73104dd8eb8fc2
unknown <ch.kanoy@gmail.com>
2019-10-11 18:41:42 -0500

    finished project

--- server.c
+++ server.c
@@ -17,44 +17,30 @@ void error(const char *msg)
 void *handleThread(int mysockfd)
 {
     int n;
-    char buffer[256]; //lets server read characters from socket connection into buffer
-    int sockfd, newsockfd, portno;
-
-    socklen_t clilen;	//stores size of client for system calls
-    struct sockaddr_in serv_addr, cli_addr;
-    pthread_t pth; //thread identifier
+    char buffer[256];
 
     //LOOP
-    while(strcmp(buffer, "EXIT\n") != 0)
+    while(1)
     {
-        void *ret;
-        //sleep(1); //give main enough time to get another client if it needs to
-
         bzero(buffer,256);
         n = read(mysockfd,buffer,255); //reads from socket
+
         if (n < 0)
             error("ERROR reading from socket"); //produces error message if error code is present; stops code
-        if(strcmp(buffer, "EXIT\n") != 0)
+        if(strcmp(buffer, "") == 0)
         {
-            printf("Here is the message from client %d: %s\n",mysockfd, buffer); //prints message if no error occurs
-
-            n = write(mysockfd,"I got your message",18); //writes message reception message
-            if (n < 0)
-                {
-                    error("ERROR writing to socket"); //produces error message if error code is present
-                 }
-        }
-        else{
-
             close(mysockfd);
             pthread_exit(NULL);
-            //return NULL;
         }
+        printf("Here is the message from client %d: %s\n",mysockfd, buffer); //prints message if no error occurs
 
+        n = write(mysockfd,"I got your message",18); //writes message reception message
+        if (n < 0)
+            error("ERROR writing to socket"); //produces error message if error code is present
     }
     //END LOOP
 
-    //return NULL;
+    return NULL;
 }
 
 int main(int argc, char* argv[])
@@ -103,9 +89,7 @@ int main(int argc, char* argv[])
     //LOOP
     while(1)
     {
-        //usleep(1); //give main enough time to get another client if it needs to
         clilen = sizeof(cli_addr);
-        printf("1\n");
         newsockfd = accept(sockfd,
                            (struct sockaddr *) &cli_addr,
                            &clilen); /*waits until proper connection has been made to a client
@@ -115,29 +99,9 @@ int main(int argc, char* argv[])
         if (newsockfd < 0)
             error("ERROR on accept"); //produces error if error code (-1) is present
 
-        /*
-        pid = fork();
-        if(pid < 0)
-            error("ERROR on fork");
-        if(pid == 0){
-            close(sockfd);
-            handleThread(newsockfd);
-            exit(0);
-        }
-        else{
-            close(newsockfd);
-        }
-        */
-
-
-        //close(sockfd);
         if(pthread_create(&pth, NULL, handleThread, newsockfd) < 0){
-           // close(sockfd);
-            
             printf("Failed to create thread\n");
         }
-        //pthread_join(pth, NULL);
-        //usleep(1); //give main enough time to get another client if it needs to
 
     }
     //END LOOP
